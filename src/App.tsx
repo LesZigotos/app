5a2db78ab22a855f47e4a712efdddb0654784676
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2024-12-05 21:02:19 +0100

    ➕ Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <h1 className='text-8xl text-red-700'>Hello Tailwind</h1>
     </>
   )
 }
