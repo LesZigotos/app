adcd6b9eea595ee2aaf19cefdd537d7e4343f605
Marius <marius.mignard.etu@univ-lille.fr>
2024-12-06 03:27:41 +0100

    Systeme de point fonctionnel

--- src/page/quizz-page.tsx
+++ src/page/quizz-page.tsx
@@ -15,18 +15,32 @@ function QuizzPage() {
     fertility: 85,
   });
 
+  const [recentChanges, setRecentChanges] = useState({
+    heartRate: 0,
+    immuneSystem: 0,
+    bodyTemperature: 0,
+    ph: 0,
+    bloodPressure: 0,
+    respiratory: 0,
+    fertility: 0,
+  });
+
   return (
     <div className="h-full bg-gray overflow-scroll text-primary-darker">
       <Header />
       <div className="flex max-lg:flex-col justify-around items-center p-12 gap-12">
         <div className="flex flex-col justify-center">
-          <VitalityIndicatorsBox indicators={vitalityIndicators} />
+          <VitalityIndicatorsBox
+            indicators={vitalityIndicators}
+            recentChanges={recentChanges}
+          />
           <img src="/perso.png" className="max-lg:max-w-[400px]" />
         </div>
         <div>
           <QuizComponent
             vitalityIndicators={vitalityIndicators}
             setVitalityIndicators={setVitalityIndicators}
+            setRecentChanges={setRecentChanges}
           />
         </div>
       </div>
