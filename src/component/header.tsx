a3e711d64549a14d779ae316eaa94069c7e9ae05
Luce <luce@pop-os.localdomain>
2024-12-06 02:21:07 +0100

    <add> burger-menu

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,14 +1,22 @@
 import { useNavigate } from "react-router-dom"
+import BurgerMenu from "./menu";
+import { useState } from "react";
 
 function Header() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [open, setOpen] = useState<boolean>(false);
 
     return (
-        <div className="px-4 bg-primary-darker flex justify-around max-lg:justify-between items-center h-20 max-md:h-16 w-full">
-            <img onClick={() => navigate('/')} src="logo.png" className="h-2/4 cursor-pointer" />
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="h-2/4">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
+        <div>
+            <div className="px-4 bg-primary-darker flex justify-around max-lg:justify-between items-center h-20 max-md:h-16 w-full">
+                <img onClick={() => navigate('/')} src="logo.png" className="h-2/4 cursor-pointer" />
+                <button onClick={() => setOpen(true)} className="h-2/4">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="h-full" >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+            </div>
+            <BurgerMenu open={open} onClose={() => setOpen(false)} />
         </div>
     )
 }
