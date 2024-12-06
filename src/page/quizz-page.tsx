c194e3421b4857645af3fd8060b14a06a58cc6ee
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2024-12-06 02:03:13 +0100

    🔥 Remove useless header and footer

--- src/page/quizz-page.tsx
+++ src/page/quizz-page.tsx
@@ -1,5 +1,3 @@
-import Footer from "../component/footer"
-import Header from "../component/header"
 import QuizCard from "../component/quizz-card"
 import VitalityIndicatorsBox from "../component/vitality-indicators-box"
 
@@ -7,7 +5,6 @@ function QuizzPage() {
 
     return (
         <div className="h-full bg-gray overflow-scroll text-primary-darker" >
-            <Header />
             <div className="flex max-lg:flex-col justify-around items-center p-12 gap-12">
                 <div className="flex flex-col justify-center">
                     <VitalityIndicatorsBox />
@@ -17,7 +14,6 @@ function QuizzPage() {
                     <QuizCard />
                 </div>
             </div>
-            <Footer />
         </div>
     )
 }
